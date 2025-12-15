98af3c7a243259e95794d9dec78678e0f6b1dbb5
Your Name <your.email@example.com>
2025-12-15 17:16:03 +0530

    Force dynamic shorturl redirect (fix caching issue)

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -1,5 +1,20 @@
-import { redirect } from "next/navigation";
+export const dynamic = "force-dynamic";
 
-export default function Page({ params }) {
-  redirect("https://google.com");
+import { redirect } from "next/navigation";
+import clientPromise from "@/lib/mongodb";
+
+export default async function Page({ params }) {
+  const { shorturl } = params;
+
+  const client = await clientPromise;
+  const db = client.db("bitlinks");
+  const collection = db.collection("url");
+
+  const doc = await collection.findOne({ shorturl });
+
+  if (!doc) {
+    redirect("/");
+  }
+
+  redirect(doc.url);
 }
